6a3b08aa54fdc9d7b6c43c51ac867abb58b401a5
R4yya <121268707+R4yya@users.noreply.github.com>
2023-12-08 18:08:30 +0300

    update quiz models

--- cyber_quest/quiz/models.py
+++ cyber_quest/quiz/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_id = models.AutoField(primary_key=True)
+    topic = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=50)
+    age_orientation = models.CharField(max_length=50)
+    
+    
+    def __str__(self):
+        return f"{self.topic} - {self.difficulty}"
+
+class Answer(models.Model):
+    answer_id = models.AutoField(primary_key=True)
+    question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE)
+    text = models.TextField()
+    is_correct = models.BooleanField(default=False)
+    
+
+    def __str__(self):
+        return f"{self.question.topic} - {self.text} (Correct: {self.is_correct})"
