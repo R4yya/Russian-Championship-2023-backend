67df4967d726d7bd216d31207055cbb925e981cb
R4yya <121268707+R4yya@users.noreply.github.com>
2023-12-09 09:34:10 +0300

    apply jwtoken authentication

--- cyber_quest/user_api/urls.py
+++ cyber_quest/user_api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
@@ -6,4 +7,5 @@ urlpatterns = [
 	path('login', views.UserLogin.as_view(), name='login'),
 	path('logout', views.UserLogout.as_view(), name='logout'),
 	path('user', views.UserView.as_view(), name='user'),
+	path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
--- cyber_quest/user_api/views.py
+++ cyber_quest/user_api/views.py
@@ -5,10 +5,12 @@ from rest_framework.response import Response
 from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
 from rest_framework import permissions, status
 from .validations import custom_validation, validate_email, validate_password
-
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 class UserRegister(APIView):
-    permission_classes = (permissions.AllowAny,)
+    permission_classes = (AllowAny,)
 
 
     def post(self, request):
@@ -18,14 +20,16 @@ class UserRegister(APIView):
         if serializer.is_valid(raise_exception=True):
             user = serializer.create(clean_data)
             if user:
-                return Response({'user': serializer.data}, status=status.HTTP_201_CREATED)
+                token = RefreshToken.for_user(user)
+                data = serializer.data
+                data["tokens"] = {"refresh": str(token), "access": str(token.access_token)}
+                return Response(data, status=status.HTTP_201_CREATED)
                 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserLogin(APIView):
-    permission_classes = (permissions.AllowAny,)
-    authentication_classes = (SessionAuthentication,)
+    permission_classes = (AllowAny,)
 
 
     def post(self, request):
@@ -41,23 +45,31 @@ class UserLogin(APIView):
             user = serializer.check_user(data)
             login(request, user)
 
-            return Response({'user': serializer.data}, status=status.HTTP_200_OK)
+            token = RefreshToken.for_user(user)
+            data = serializer.data
+            data["tokens"] = {"refresh": str(token), "access": str(token.access_token)}
+            return Response(data, status=status.HTTP_200_OK)
 
 
 class UserLogout(APIView):
-    permission_classes = (permissions.AllowAny,)
-    authentication_classes = ()
+    permission_classes = (AllowAny,)
 
 
     def post(self, request):
         logout(request)
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(status=status.HTTP_200_OK)
 
 
 class UserView(APIView):
-    permission_classes = (permissions.IsAuthenticated,)
-    authentication_classes = (SessionAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
 
 
     def get(self, request):
